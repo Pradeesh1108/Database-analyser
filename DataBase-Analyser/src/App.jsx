6354d0252928fd165b2213972f823f7b258b880a
SaiDhinakar <saidhinakars899@gmail.com>
2025-03-02 18:39:45 +0530

    Initial commit

---
+++ DataBase-Analyser/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="title">
+        <h1>DataBase Analyser</h1>
+      </div>
+      <span>
+        <h3>Developers :</h3>
+        <div>S.SaiDhinakar  - Full Stack developer</div>
+        <div>S.Pradeesh - Python developer</div>
+      </span>
+    </>
+  );
+}
+
+export default App;
