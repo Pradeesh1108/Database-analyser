27f2c5b8405230630c8d8dc241c3b888aee8c80e
SaiDhinakar <saidhinakars899@gmail.com>
2025-03-07 22:28:54 +0530

    Code update

--- DataBase-Analyser/src/App.jsx
+++ DataBase-Analyser/src/App.jsx
@@ -1,16 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <div className="title">
-        <h1>DataBase Analyser</h1>
-      </div>
-      <span>
-        <h3>Developers :</h3>
-        <div>S.SaiDhinakar  - Full Stack developer</div>
-        <div>S.Pradeesh - Python developer</div>
-      </span>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
     </>
   );
 }
---
+++ DataBase-Analyser/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "../css/header.css";
+
+const Header = () => {
+  return (
+    <div className="flex">
+      <div className="w-14 flex-none ...">01</div>
+      <div className="w-64 flex-1 ...">02</div>
+      <div className="w-32 flex-1 ...">03</div>
+    </div>
+  );
+};
+
+export default Header;
